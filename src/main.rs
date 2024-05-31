71a3f266ebf1cdfe033ebef140ebf66d4056528a
jinx <jinx-eob1@proton.me>
2024-05-31 16:04:14 -0500

    Remove tokio-websockets in favor of axum's
    * We're already using axum, so might as well double down

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::{Result, Context};
+use axum::extract::State;
+use axum::extract::ws::{WebSocketUpgrade, WebSocket};
 use clap::Parser;
 use futures_util::{SinkExt, StreamExt};
 use http::QueuePacket;
@@ -64,67 +66,56 @@ async fn serve_tts(mut rx: broadcast::Receiver<http::QueuePacket>, tx: broadcast
     }
 }
 
-async fn serve_http(tx: broadcast::Sender<QueuePacket>, port: u32) -> Result<()> {
+async fn handle_websocket(socket: WebSocket, rx: Arc<broadcast::Receiver<QueuePacket>>) {
+    let (mut writer, mut reader) = socket.split();
+    let alive_reader = Arc::new(atomic::AtomicBool::new(true));
+    let alive_write = Arc::clone(&alive_reader);
+
+    tokio::spawn(async move {
+        while let Some(Ok(_)) = reader.next().await { }
+        alive_reader.store(false, atomic::Ordering::Relaxed);
+    });
+
+    let mut rx = rx.resubscribe();
+    tokio::spawn(async move {
+        loop {
+            let msg = rx.recv().await;
+
+            let alive = alive_write.load(atomic::Ordering::Relaxed);
+            if !alive {
+                break;
+            }
+    
+            if let Ok(msg) = msg {
+                let tokio_msg = axum::extract::ws::Message::Text(msg.translated_text);
+
+                // Assume socket is closed
+                if let Err(_err) = writer.send(tokio_msg).await {
+                    break;
+                }
+            }
+        }
+    });
+}
+
+async fn ws_handler(ws: WebSocketUpgrade, State(rx): State<Arc<broadcast::Receiver<QueuePacket>>>) -> axum::response::Response {
+    ws.on_upgrade(|socket| handle_websocket(socket, rx))
+}
+
+async fn serve_http(rx: broadcast::Receiver<QueuePacket>, tx: broadcast::Sender<QueuePacket>, port: u32) -> Result<()> {
     let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
 
+    let rx = Arc::new(rx);
+
     let router = axum::Router::new()
+        .route("/ws", axum::routing::get(ws_handler))
+            .with_state(rx)
         .route("/queue", axum::routing::post(http::handler))
             .with_state(tx.clone());
 
     axum::serve(listener, router).await?;
 
     Ok(())
-}
-
-async fn serve_websocket(rx: broadcast::Receiver<QueuePacket>, port: u32) -> Result<()> {
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
-
-    loop {
-        let (socket, _) = match listener.accept().await {
-            Ok(v) => v,
-            Err(err) => {
-                warn!("Failed to accept tcp connection {}", err);
-                continue;
-            }
-        };
-        let ws_stream = match tokio_websockets::ServerBuilder::new().accept(socket).await {
-            Ok(v) => v,
-            Err(err) => {
-                warn!("Failed to make WS connection {}", err);
-                continue;
-            }
-        };
-
-        let (mut writer, mut reader) = ws_stream.split();
-        let alive_reader = Arc::new(atomic::AtomicBool::new(true));
-        let alive_write = Arc::clone(&alive_reader);
-
-        tokio::spawn(async move {
-            while let Some(Ok(_)) = reader.next().await { }
-            alive_reader.store(false, atomic::Ordering::Relaxed);
-        });
-
-        let mut rx = rx.resubscribe();
-        tokio::spawn(async move {
-            loop {
-                let msg = rx.recv().await;
-
-                let alive = alive_write.load(atomic::Ordering::Relaxed);
-                if !alive {
-                    break;
-                }
-        
-                if let Ok(msg) = msg {
-                    let tokio_msg = tokio_websockets::Message::text(msg.translated_text);
-
-                    // Assume socket is closed
-                    if let Err(_err) = writer.send(tokio_msg).await {
-                        break;
-                    }
-                }
-            }
-        });
-    }
 }
 
 #[derive(clap::Parser)]
@@ -163,11 +154,7 @@ async fn main() ->  Result<()> {
     let queue_tts_rx = queue_rx;
 
     let http_server = tokio::spawn(async move {
-        return serve_http(queue_tx, args.http_port.unwrap()).await;
-    });
-
-    let ws_server = tokio::spawn(async move {
-        return serve_websocket(queue_ws_rx, args.ws_port.unwrap()).await;
+        return serve_http(queue_ws_rx, queue_tx, args.http_port.unwrap()).await;
     });
 
     let tts_generator = tokio::spawn(async move {
@@ -180,7 +167,6 @@ async fn main() ->  Result<()> {
 
     let res = tokio::select! {
         res = http_server   => res.unwrap(),
-        res = ws_server     => res.unwrap(),
         res = audio_reader  => res.unwrap(),
         res = tts_generator => res.unwrap()
     };
