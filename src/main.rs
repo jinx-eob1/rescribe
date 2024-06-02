aa5bb825b00e2fe614bbae3b94a25e05e14be199
jinx <jinx-eob1@proton.me>
2024-06-01 23:23:18 -0500

    Move ui endpoint to root

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ async fn serve_http(rx: broadcast::Receiver<QueuePacket>, tx: broadcast::Sender<
     let rx = Arc::new(rx);
 
     let router = axum::Router::new()
-        .nest_service("/ui", ServeDir::new("ui"))
+        .nest_service("/", ServeDir::new("ui"))
         .route("/ws",    axum::routing::get (http::handle_websocket)).with_state(rx)
         .route("/queue", axum::routing::post(http::handle_post))     .with_state(tx);
 
